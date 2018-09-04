9c2b2990e88c62bf3c613b592309ffa2dd1852a0
Jeremy Williamson <jeremy.williamson>
2018-09-03 21:11:11 -0400

    Able to get fighter links from a page

--- scraper.py
+++ scraper.py
@@ -10,30 +10,35 @@ def get_soup(current_page):
 root_page = "https://www.mixedmartialarts.com/fighter"
 
 #for testing
-def setup_root_page(current_page, file_name):
+def setup_root_page(current_page):
     soup = get_soup(current_page)
-    with open(file_name, "w", encoding="utf8") as file:
-        list_of_link_lines = soup.find_all('a')
-        for line in list_of_link_lines:
-            file.write(line.prettify())
+    list_of_link_lines = soup.find_all('a')
+    for line in list_of_link_lines:
+        yield line.prettify()
 
-def get_fighter_links(file_name):
-    pattern = re.compile(r'<a href=\"\/*fighter\/[A-Za-z]*-[A-Za-z]*:[A-Z0-9]*\">')
-    links = []
-    with open(file_name, 'r', encoding='utf8') as file:
-        for line in file:
-            match = re.search(pattern, line)
-            if match and match.group(0) not in links:
-                links.append(match.group(0))
+def get_fighter_links(raw_links, links):
+    # pattern = re.compile(r'<a href=\"\/*fighter\/[A-Za-z]*-[A-Za-z]*:[A-Z0-9]*\">')
+    pattern = re.compile(r'\/?fighter\/[A-Za-z]*-[A-Za-z]*:[A-Z0-9]*')
+    for line in raw_links:
+        match = re.search(pattern, line)
+        if match and match.group(0) not in links:
+            links.append(match.group(0))
+
     return links
 
 def main():
     #setup content file
-    file_name = "output.html"
-    setup_root_page(root_page, file_name)
-    fighter_links = get_fighter_links(file_name)
+    url = "https://www.mixedmartialarts.com/"
+    # just the a href link
+    links = []
+    # the fully formed url from links
+    url_list = []
+    fighter_links = get_fighter_links(setup_root_page(root_page), links)
     for fighter in fighter_links:
-        print(fighter)
+        url_list.append(url + fighter)
+
+    for link in url_list:
+        print(link)
 
 if __name__ == "__main__":
     main()
